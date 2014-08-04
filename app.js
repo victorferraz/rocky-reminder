f1faf2d6d56b8623ecd8ff71a34b89be0ec6e0e8
Victor Ferraz <cashrox@gmail.com>
2014-08-04 18:55:13 -0300

    add schedule to run in background

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+'use strict';
 chrome.app.runtime.onLaunched.addListener(function () {
-  chrome.app.window.create("window.html", {
-    id: "mainwin",
-    bounds: {width:600, height:400}
-  });
+    chrome.app.window.create('window.html', {
+        id: 'mainwin',
+        bounds: {width:600, height:400}
+    });
+});
+var NAME = 'teste';
+var alarmInfo = {
+    'delayInMinutes':1,
+    'periodInMinutes':10
+};
+
+chrome.alarms.create(NAME, alarmInfo);
+chrome.alarms.onAlarm.addListener(function(){
+    new NotifyReminder();
 });
--- main.js
+++ main.js
@@ -7,7 +7,6 @@ var NotifyReminder = function(){
 NotifyReminder.prototype.init = function () {
     this.notifyID = 0;
     this.showMessage();
-    console.log('teste');
 };
 
 NotifyReminder.prototype.addListeners = function() {
@@ -15,26 +14,44 @@ NotifyReminder.prototype.addListeners = function() {
 
 
 NotifyReminder.prototype.messages = [
-        {
-            type: 'basic',
-            title: 'Rocky Balboa Inspiration',
-            message: 'It ain\'t about how hard you can hit. Its about how hard you can get hit, and how much you can take, and keep moving forward.',
-            expandedMessage: 'It ain\'t about how hard you can hit. Its about how hard you can get hit, and how much you can take, and keep moving forward.',
-            iconUrl: '/images/inbox-64x64.png',
-        },
-    ];
+    {
+        type: 'basic',
+        title: 'Rocky Balboa Inspiration',
+        message: 'It ain\'t about how hard you can hit. Its about how hard you can get hit, and how much you can take, and keep moving forward.',
+        expandedMessage: 'It ain\'t about how hard you can hit. Its about how hard you can get hit, and how much you can take, and keep moving forward.',
+        iconUrl: '/images/rocky.png',
+    },{
+        type: 'basic',
+        title: 'Uncle Arnold Inspiration',
+        message: 'The last three or four reps is what makes the muscle grow. This area of pain divides the champion from someone else who is not a champion.',
+        expandedMessage: 'The last three or four reps is what makes the muscle grow. This area of pain divides the champion from someone else who is not a champion.',
+        iconUrl: '/images/arnold.png',
+    },{
+        type: 'basic',
+        title: 'Uncle Arnold Inspiration',
+        message: 'Start wide, expand further, and never look back.',
+        expandedMessage: 'Start wide, expand further, and never look back.',
+        iconUrl: '/images/arnold.png',
+    },{
+        type: 'basic',
+        title: 'Muhammed Ali',
+        message: 'I don’t count my sit-ups. I only start counting when it starts hurting. When I feel pain, that’s when I start counting, because that’s when it really counts.',
+        expandedMessage: 'I don’t count my sit-ups. I only start counting when it starts hurting. When I feel pain, that’s when I start counting, because that’s when it really counts.',
+        iconUrl: '/images/arnold.png',
+    },
+];
+
+NotifyReminder.prototype.random = function(){
+
+}
 
 NotifyReminder.prototype.showMessage = function(){
     var options = this.messages[0];
     options.priority = 1;
     options.buttons = [];
     this.notifyid = this.notifyID++;
-    console.log( options );
     chrome.notifications.create('id'+this.notifyID, options, this.hideMessage);
 };
 
 NotifyReminder.prototype.hideMessage = function(){
-    console.log('teste');
 };
-
-new NotifyReminder();
